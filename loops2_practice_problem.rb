84cf8fad9680aed50c1d6c991d67f54b0c2b3cca
Jonathan <jonitoadriano@gmail.com>
2022-12-22 23:26:30 -0600

    done ex5 to 10

--- loops2_practice_problem.rb
+++ loops2_practice_problem.rb
@@ -23,20 +23,57 @@
 # end
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
-i = 0
-while i != 105
-  puts i
-  i += 5
-end
+# i = 0
+# while i != 105
+#   puts i
+#   i += 5
+# end
 
 # 5. Write a while loop that prints the number 9000 ten times.
+# i = 0
+# while i < 10
+#   puts "9000"
+#   i += 1
+# end
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+# puts "Number"
+# num = gets.chomp
+# num = num.to_i
+
+# while num <= 10
+#   puts "Try another number"
+#   num = gets.chomp
+#   num = num.to_i
+# end 
 
 # 7. Write a while loop that prints the numbers 50 to 70.
+# i = 50
+# while i < 71
+#   puts i
+#   i += 1
+# end
+
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+# i = 0
+# while i < 144
+#   puts "Around the world"
+#   i += 1
+# end
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+# puts "enter a word"
+# word = gets.chomp
+
+# while word.length < 5
+#   puts "try another"
+#   word = gets.chomp
+# end
 
 # 10. Write a while loop that prints the even numbers from 2 to 40.
+# i = 2
+# while i < 42
+#   puts i
+#   i += 2
+# end
