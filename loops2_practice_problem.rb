bc126fcfc7e04f3dadab3fcfe34d36073ebd2172
Jonathan <jonitoadriano@gmail.com>
2022-12-22 23:10:55 -0600

    done ex4

--- loops2_practice_problem.rb
+++ loops2_practice_problem.rb
@@ -23,6 +23,11 @@
 # end
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+i = 0
+while i != 105
+  puts i
+  i += 5
+end
 
 # 5. Write a while loop that prints the number 9000 ten times.
 
